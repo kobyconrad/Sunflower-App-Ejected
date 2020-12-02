0892689978493089f2cb46d45cd1603b486b91ae
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-12-02 15:26:01 -0800

    new sunflowers grow for every day sober

--- App.js
+++ App.js
@@ -125,15 +125,136 @@ function HomeScreen({ navigation }) {
     }
   }
 
-  let imageOne = require(`./assets/sunflowers.jpg`);
-  let imageTwo = require(`./assets/beach.png`);
-
   let mySource;
 
-  if (time.days > 1) {
-    mySource = imageOne;
+  if (time.days < 1) {
+    mySource = require(`./assets/backgrounds/sunflowers-62.png`);
+  } else if (time.days < 2) {
+    mySource = require(`./assets/backgrounds/sunflowers-61.png`);
+  } else if (time.days < 3) {
+    mySource = require(`./assets/backgrounds/sunflowers-60.png`);
+  } else if (time.days < 4) {
+    mySource = require(`./assets/backgrounds/sunflowers-59.png`);
+  } else if (time.days < 5) {
+    mySource = require(`./assets/backgrounds/sunflowers-58.png`);
+  } else if (time.days < 6) {
+    mySource = require(`./assets/backgrounds/sunflowers-57.png`);
+  } else if (time.days < 7) {
+    mySource = require(`./assets/backgrounds/sunflowers-56.png`);
+  } else if (time.days < 8) {
+    mySource = require(`./assets/backgrounds/sunflowers-55.png`);
+  } else if (time.days < 9) {
+    mySource = require(`./assets/backgrounds/sunflowers-54.png`);
+  } else if (time.days < 10) {
+    mySource = require(`./assets/backgrounds/sunflowers-53.png`);
+  } else if (time.days < 11) {
+    mySource = require(`./assets/backgrounds/sunflowers-52.png`);
+  } else if (time.days < 12) {
+    mySource = require(`./assets/backgrounds/sunflowers-51.png`);
+  } else if (time.days < 13) {
+    mySource = require(`./assets/backgrounds/sunflowers-50.png`);
+  } else if (time.days < 14) {
+    mySource = require(`./assets/backgrounds/sunflowers-49.png`);
+  } else if (time.days < 15) {
+    mySource = require(`./assets/backgrounds/sunflowers-48.png`);
+  } else if (time.days < 16) {
+    mySource = require(`./assets/backgrounds/sunflowers-47.png`);
+  } else if (time.days < 17) {
+    mySource = require(`./assets/backgrounds/sunflowers-46.png`);
+  } else if (time.days < 18) {
+    mySource = require(`./assets/backgrounds/sunflowers-45.png`);
+  } else if (time.days < 19) {
+    mySource = require(`./assets/backgrounds/sunflowers-44.png`);
+  } else if (time.days < 20) {
+    mySource = require(`./assets/backgrounds/sunflowers-43.png`);
+  } else if (time.days < 21) {
+    mySource = require(`./assets/backgrounds/sunflowers-42.png`);
+  } else if (time.days < 22) {
+    mySource = require(`./assets/backgrounds/sunflowers-41.png`);
+  } else if (time.days < 23) {
+    mySource = require(`./assets/backgrounds/sunflowers-40.png`);
+  } else if (time.days < 24) {
+    mySource = require(`./assets/backgrounds/sunflowers-39.png`);
+  } else if (time.days < 25) {
+    mySource = require(`./assets/backgrounds/sunflowers-38.png`);
+  } else if (time.days < 26) {
+    mySource = require(`./assets/backgrounds/sunflowers-37.png`);
+  } else if (time.days < 27) {
+    mySource = require(`./assets/backgrounds/sunflowers-36.png`);
+  } else if (time.days < 28) {
+    mySource = require(`./assets/backgrounds/sunflowers-35.png`);
+  } else if (time.days < 29) {
+    mySource = require(`./assets/backgrounds/sunflowers-34.png`);
+  } else if (time.days < 30) {
+    mySource = require(`./assets/backgrounds/sunflowers-33.png`);
+  } else if (time.days < 31) {
+    mySource = require(`./assets/backgrounds/sunflowers-32.png`);
+  } else if (time.days < 32) {
+    mySource = require(`./assets/backgrounds/sunflowers-31.png`);
+  } else if (time.days < 33) {
+    mySource = require(`./assets/backgrounds/sunflowers-30.png`);
+  } else if (time.days < 34) {
+    mySource = require(`./assets/backgrounds/sunflowers-29.png`);
+  } else if (time.days < 35) {
+    mySource = require(`./assets/backgrounds/sunflowers-28.png`);
+  } else if (time.days < 36) {
+    mySource = require(`./assets/backgrounds/sunflowers-27.png`);
+  } else if (time.days < 37) {
+    mySource = require(`./assets/backgrounds/sunflowers-26.png`);
+  } else if (time.days < 38) {
+    mySource = require(`./assets/backgrounds/sunflowers-25.png`);
+  } else if (time.days < 39) {
+    mySource = require(`./assets/backgrounds/sunflowers-24.png`);
+  } else if (time.days < 40) {
+    mySource = require(`./assets/backgrounds/sunflowers-23.png`);
+  } else if (time.days < 41) {
+    mySource = require(`./assets/backgrounds/sunflowers-22.png`);
+  } else if (time.days < 42) {
+    mySource = require(`./assets/backgrounds/sunflowers-21.png`);
+  } else if (time.days < 43) {
+    mySource = require(`./assets/backgrounds/sunflowers-20.png`);
+  } else if (time.days < 44) {
+    mySource = require(`./assets/backgrounds/sunflowers-19.png`);
+  } else if (time.days < 45) {
+    mySource = require(`./assets/backgrounds/sunflowers-18.png`);
+  } else if (time.days < 46) {
+    mySource = require(`./assets/backgrounds/sunflowers-17.png`);
+  } else if (time.days < 47) {
+    mySource = require(`./assets/backgrounds/sunflowers-16.png`);
+  } else if (time.days < 48) {
+    mySource = require(`./assets/backgrounds/sunflowers-15.png`);
+  } else if (time.days < 49) {
+    mySource = require(`./assets/backgrounds/sunflowers-14.png`);
+  } else if (time.days < 50) {
+    mySource = require(`./assets/backgrounds/sunflowers-13.png`);
+  } else if (time.days < 51) {
+    mySource = require(`./assets/backgrounds/sunflowers-12.png`);
+  } else if (time.days < 52) {
+    mySource = require(`./assets/backgrounds/sunflowers-11.png`);
+  } else if (time.days < 53) {
+    mySource = require(`./assets/backgrounds/sunflowers-10.png`);
+  } else if (time.days < 54) {
+    mySource = require(`./assets/backgrounds/sunflowers-9.png`);
+  } else if (time.days < 55) {
+    mySource = require(`./assets/backgrounds/sunflowers-8.png`);
+  } else if (time.days < 56) {
+    mySource = require(`./assets/backgrounds/sunflowers-7.png`);
+  } else if (time.days < 57) {
+    mySource = require(`./assets/backgrounds/sunflowers-6.png`);
+  } else if (time.days < 58) {
+    mySource = require(`./assets/backgrounds/sunflowers-5.png`);
+  } else if (time.days < 59) {
+    mySource = require(`./assets/backgrounds/sunflowers-4.png`);
+  } else if (time.days < 60) {
+    mySource = require(`./assets/backgrounds/sunflowers-3.png`);
+  } else if (time.days < 61) {
+    mySource = require(`./assets/backgrounds/sunflowers-2.png`);
+  } else if (time.days < 62) {
+    mySource = require(`./assets/backgrounds/sunflowers-1.png`);
+  } else if (time.days >= 62) {
+    mySource = require(`./assets/backgrounds/sunflowers-0.png`);
   } else {
-    mySource = imageTwo;
+    mySource = require(`./assets/backgrounds/sunflowers-62.png`);
   }
 
   return (
@@ -152,13 +273,13 @@ function HomeScreen({ navigation }) {
           position: "absolute",
           height: "100%",
           width: "100%",
-          opacity: 0.25,
+          opacity: 0.2,
         }}
       ></View>
 
       <View style={styles.headerContainer}>
         <Image
-          source={require(`./assets/sunflower-logo.png`)}
+          source={require(`./assets/sunflower-logo-v2.png`)}
           style={{ height: "100%", width: "50%", marginTop: 34 }}
           resizeMode="contain"
         />
@@ -176,7 +297,7 @@ function HomeScreen({ navigation }) {
       </View>
 
       <Image
-        source={require("./assets/clock-square.png")}
+        source={require("./assets/clock.gif")}
         resizeMode={"contain"}
         style={{ width: "60%", marginTop: 40 }}
       />
