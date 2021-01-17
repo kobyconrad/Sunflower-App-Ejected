52f13d03f4e025d616c3276c98f32fa5ab133b1b
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2021-01-17 12:27:29 -0800

    passes props to determine style of activity item select

--- components/journal/activityLogExercise.js
+++ components/journal/activityLogExercise.js
@@ -13,6 +13,21 @@ import { useState } from "react";
 
 function ActivityLogExercise(props) {
   const [screen, setScreen] = useState(0);
+  const [exerciseStyle, setExerciseStyle] = useState(true);
+
+  let touchProps = {
+    style: exerciseStyle
+      ? styles.activityItemContainer
+      : styles.activityItemContainerOn,
+  };
+  let touchPropsBody = {
+    style: exerciseStyle
+      ? styles.activityItemBodyContainer
+      : styles.activityItemBodyContainerOn,
+  };
+  let touchPropsBodyText = {
+    style: exerciseStyle ? styles.paragraphText : styles.paragraphTextOn,
+  };
 
   let NavJournalScreen = props.NavJournalScreen;
 
@@ -32,120 +47,156 @@ function ActivityLogExercise(props) {
         >
           <ScrollView style={{}} contentContainerStyle={{}}>
             <View style={{}}>
-              <View style={styles.activityItemContainer}>
-                <View style={styles.activityTitleContainer}>
-                  <Image
-                    source={require(`./../../assets/arm-emoji.png`)}
-                    style={{ width: 16, height: 16, marginRight: 6 }}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: "800",
-                      color: "#000100",
-                    }}
-                  >
-                    Exercise
-                  </Text>
+              <TouchableHighlight
+                onPress={() => {
+                  Haptics.selectionAsync();
+                  console.log(exerciseStyle);
+                  let current = exerciseStyle;
+                  setExerciseStyle(!current);
+                }}
+                underlayColor=""
+                style={{ width: "100%", display: "flex" }}
+                activeOpacity={0.6}
+              >
+                <View {...touchProps}>
+                  <View style={styles.activityTitleContainer}>
+                    <Image
+                      source={require(`./../../assets/arm-emoji.png`)}
+                      style={{ width: 16, height: 16, marginRight: 6 }}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: "800",
+                        color: "#000100",
+                      }}
+                    >
+                      Exercise
+                    </Text>
+                  </View>
+                  <View {...touchPropsBody}>
+                    <Text {...touchPropsBodyText}>
+                      ex: Going for a run, lifting weights, practicing sports.
+                    </Text>
+                  </View>
                 </View>
-                <View style={styles.activityItemBodyContainer}>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      fontWeight: "400",
-                      color: "#000100",
-                    }}
-                  >
-                    ex: Going for a run, lifting weights, practicing sports.
-                  </Text>
+              </TouchableHighlight>
+
+              <TouchableHighlight
+                onPress={() => {
+                  Haptics.selectionAsync();
+                }}
+                underlayColor=""
+                style={{ width: "100%", display: "flex" }}
+                activeOpacity={0.6}
+              >
+                <View style={styles.activityItemContainer}>
+                  <View style={styles.activityTitleContainer}>
+                    <Image
+                      source={require(`./../../assets/family-emoji.png`)}
+                      style={{ width: 16, height: 16, marginRight: 6 }}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: "800",
+                        color: "#000100",
+                      }}
+                    >
+                      Social Activity
+                    </Text>
+                  </View>
+                  <View style={styles.activityItemBodyContainer}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: "400",
+                        color: "#000100",
+                      }}
+                    >
+                      ex: Spending time with family, friends, or loved ones.
+                    </Text>
+                  </View>
                 </View>
-              </View>
-              <View style={styles.activityItemContainer}>
-                <View style={styles.activityTitleContainer}>
-                  <Image
-                    source={require(`./../../assets/family-emoji.png`)}
-                    style={{ width: 16, height: 16, marginRight: 6 }}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: "800",
-                      color: "#000100",
-                    }}
-                  >
-                    Social Activity
-                  </Text>
+              </TouchableHighlight>
+
+              <TouchableHighlight
+                onPress={() => {
+                  Haptics.selectionAsync();
+                }}
+                underlayColor=""
+                style={{ width: "100%", display: "flex" }}
+                activeOpacity={0.6}
+              >
+                <View style={styles.activityItemContainer}>
+                  <View style={styles.activityTitleContainer}>
+                    <Image
+                      source={require(`./../../assets/work-emoji.png`)}
+                      style={{ width: 16, height: 16, marginRight: 6 }}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: "800",
+                        color: "#000100",
+                      }}
+                    >
+                      Rewarding Work
+                    </Text>
+                  </View>
+                  <View style={styles.activityItemBodyContainer}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: "400",
+                        color: "#000100",
+                      }}
+                    >
+                      ex: Any work that you feel proud of, or gave you a sense
+                      of purpose.
+                    </Text>
+                  </View>
                 </View>
-                <View style={styles.activityItemBodyContainer}>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      fontWeight: "400",
-                      color: "#000100",
-                    }}
-                  >
-                    ex: Spending time with family, friends, or loved ones.
-                  </Text>
+              </TouchableHighlight>
+
+              <TouchableHighlight
+                onPress={() => {
+                  Haptics.selectionAsync();
+                }}
+                underlayColor=""
+                style={{ width: "100%", display: "flex" }}
+                activeOpacity={0.6}
+              >
+                <View style={styles.activityItemContainer}>
+                  <View style={styles.activityTitleContainer}>
+                    <Image
+                      source={require(`./../../assets/books-emoji.png`)}
+                      style={{ width: 16, height: 16, marginRight: 6 }}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 16,
+                        fontWeight: "800",
+                        color: "#000100",
+                      }}
+                    >
+                      Learning New Things
+                    </Text>
+                  </View>
+                  <View style={styles.activityItemBodyContainer}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: "400",
+                        color: "#000100",
+                      }}
+                    >
+                      ex: Anything you learned that was pleasurable and brought
+                      joy.
+                    </Text>
+                  </View>
                 </View>
-              </View>
-              <View style={styles.activityItemContainer}>
-                <View style={styles.activityTitleContainer}>
-                  <Image
-                    source={require(`./../../assets/work-emoji.png`)}
-                    style={{ width: 16, height: 16, marginRight: 6 }}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: "800",
-                      color: "#000100",
-                    }}
-                  >
-                    Rewarding Work
-                  </Text>
-                </View>
-                <View style={styles.activityItemBodyContainer}>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      fontWeight: "400",
-                      color: "#000100",
-                    }}
-                  >
-                    ex: Any work that you feel proud of, or gave you a sense of
-                    purpose.
-                  </Text>
-                </View>
-              </View>
-              <View style={styles.activityItemContainer}>
-                <View style={styles.activityTitleContainer}>
-                  <Image
-                    source={require(`./../../assets/books-emoji.png`)}
-                    style={{ width: 16, height: 16, marginRight: 6 }}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: "800",
-                      color: "#000100",
-                    }}
-                  >
-                    Learning New Things
-                  </Text>
-                </View>
-                <View style={styles.activityItemBodyContainer}>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      fontWeight: "400",
-                      color: "#000100",
-                    }}
-                  >
-                    ex: Anything you learned that was pleasurable and brought
-                    joy.
-                  </Text>
-                </View>
-              </View>
+              </TouchableHighlight>
             </View>
           </ScrollView>
         </SafeAreaView>
@@ -279,6 +330,17 @@ const styles = StyleSheet.create({
     padding: 14,
     marginBottom: 20,
   },
+  activityItemContainerOn: {
+    borderColor: "#6A49E8",
+    width: "100%",
+    borderWidth: 1,
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+    borderRadius: 6,
+    minHeight: 100,
+    padding: 14,
+    marginBottom: 20,
+  },
   activityItemBodyContainer: {
     width: "100%",
     backgroundColor: "#EDEFFB",
@@ -286,9 +348,26 @@ const styles = StyleSheet.create({
     padding: 8,
     marginTop: 10,
   },
+  activityItemBodyContainerOn: {
+    width: "100%",
+    backgroundColor: "#6A49E8",
+    borderRadius: 6,
+    padding: 8,
+    marginTop: 10,
+  },
   activityTitleContainer: {
     display: "flex",
     flexDirection: "row",
+  },
+  paragraphText: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: "#000100",
+  },
+  paragraphTextOn: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: "#fff",
   },
 });
 
