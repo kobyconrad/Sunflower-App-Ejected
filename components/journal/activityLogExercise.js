4650456ae665036b09b50714c1dff08ba3880fff
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2021-01-17 11:48:26 -0800

    designed activity containers and MVP flow

--- components/journal/activityLogExercise.js
+++ components/journal/activityLogExercise.js
@@ -1,48 +1,228 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  SafeAreaView,
+  ScrollView,
+  Image,
+} from "react-native";
 import * as Haptics from "expo-haptics";
+import { useState } from "react";
 
 function ActivityLogExercise(props) {
+  const [screen, setScreen] = useState(0);
+
   let NavJournalScreen = props.NavJournalScreen;
 
-  return (
-    <View style={styles.screenContainer}>
-      <Text style={styles.titleText}>What did you accomplish today?</Text>
-      <Text style={styles.subtitleText}>
-        Select any activity that was healthy for your mind or body.
-      </Text>
-      <View style={styles.exerciseNavContainer}>
-        <TouchableHighlight
-          onPress={() => {
-            Haptics.selectionAsync();
-            NavJournalScreen();
+  if (screen === 0) {
+    return (
+      <View style={styles.screenContainer}>
+        <Text style={styles.titleText}>What did you accomplish today?</Text>
+        <Text style={styles.subtitleText}>
+          Select any activity that was healthy for your mind or body.
+        </Text>
+
+        <SafeAreaView
+          style={{
+            height: "67%",
+            marginTop: 10,
           }}
-          underlayColor=""
-          style={{ width: "33%", height: "100%", display: "flex" }}
         >
-          <View style={styles.cancelContainer}>
-            <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
-              cancel
-            </Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          onPress={() => {
-            Haptics.selectionAsync();
-            // NavActivityLog0();
-          }}
-          underlayColor=""
-          style={{ width: "63%", height: "100%", display: "flex" }}
-        >
-          <View style={styles.nextContainer}>
-            <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
-              next
-            </Text>
-          </View>
-        </TouchableHighlight>
+          <ScrollView style={{}} contentContainerStyle={{}}>
+            <View style={{}}>
+              <View style={styles.activityItemContainer}>
+                <View style={styles.activityTitleContainer}>
+                  <Image
+                    source={require(`./../../assets/arm-emoji.png`)}
+                    style={{ width: 16, height: 16, marginRight: 6 }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "800",
+                      color: "#000100",
+                    }}
+                  >
+                    Exercise
+                  </Text>
+                </View>
+                <View style={styles.activityItemBodyContainer}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "400",
+                      color: "#000100",
+                    }}
+                  >
+                    ex: Going for a run, lifting weights, practicing sports.
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.activityItemContainer}>
+                <View style={styles.activityTitleContainer}>
+                  <Image
+                    source={require(`./../../assets/family-emoji.png`)}
+                    style={{ width: 16, height: 16, marginRight: 6 }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "800",
+                      color: "#000100",
+                    }}
+                  >
+                    Social Activity
+                  </Text>
+                </View>
+                <View style={styles.activityItemBodyContainer}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "400",
+                      color: "#000100",
+                    }}
+                  >
+                    ex: Spending time with family, friends, or loved ones.
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.activityItemContainer}>
+                <View style={styles.activityTitleContainer}>
+                  <Image
+                    source={require(`./../../assets/work-emoji.png`)}
+                    style={{ width: 16, height: 16, marginRight: 6 }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "800",
+                      color: "#000100",
+                    }}
+                  >
+                    Rewarding Work
+                  </Text>
+                </View>
+                <View style={styles.activityItemBodyContainer}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "400",
+                      color: "#000100",
+                    }}
+                  >
+                    ex: Any work that you feel proud of, or gave you a sense of
+                    purpose.
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.activityItemContainer}>
+                <View style={styles.activityTitleContainer}>
+                  <Image
+                    source={require(`./../../assets/books-emoji.png`)}
+                    style={{ width: 16, height: 16, marginRight: 6 }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontWeight: "800",
+                      color: "#000100",
+                    }}
+                  >
+                    Learning New Things
+                  </Text>
+                </View>
+                <View style={styles.activityItemBodyContainer}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "400",
+                      color: "#000100",
+                    }}
+                  >
+                    ex: Anything you learned that was pleasurable and brought
+                    joy.
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+
+        <View style={styles.exerciseNavContainer}>
+          <TouchableHighlight
+            onPress={() => {
+              Haptics.selectionAsync();
+              NavJournalScreen();
+            }}
+            underlayColor=""
+            style={{ width: "33%", height: "100%", display: "flex" }}
+          >
+            <View style={styles.cancelContainer}>
+              <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
+                cancel
+              </Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              Haptics.selectionAsync();
+              setScreen(1);
+            }}
+            underlayColor=""
+            style={{ width: "63%", height: "100%", display: "flex" }}
+          >
+            <View style={styles.nextContainer}>
+              <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
+                next
+              </Text>
+            </View>
+          </TouchableHighlight>
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else if (screen === 1) {
+    return (
+      <View style={styles.screenContainer}>
+        <Text>Next Screen</Text>
+        <View style={styles.exerciseNavContainer}>
+          <TouchableHighlight
+            onPress={() => {
+              Haptics.selectionAsync();
+              setScreen(0);
+            }}
+            underlayColor=""
+            style={{
+              width: "33%",
+              height: "100%",
+              display: "flex",
+            }}
+          >
+            <View style={styles.cancelContainer}>
+              <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
+                back
+              </Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              Haptics.selectionAsync();
+              setScreen(1);
+            }}
+            underlayColor=""
+            style={{ width: "63%", height: "100%", display: "flex" }}
+          >
+            <View style={styles.nextContainer}>
+              <Text style={{ fontSize: 18, color: "#fff", fontWeight: "800" }}>
+                next
+              </Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -50,6 +230,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     padding: 25,
+    backgroundColor: "#fff",
   },
   titleText: {
     fontSize: 24,
@@ -68,7 +249,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    height: 40,
+    height: 45,
     marginTop: 20,
   },
   cancelContainer: {
@@ -86,6 +267,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 6,
     height: "100%",
+  },
+  activityItemContainer: {
+    width: "100%",
+    borderWidth: 1,
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+    borderColor: "#EDEFFB",
+    borderRadius: 6,
+    minHeight: 100,
+    padding: 14,
+    marginBottom: 20,
+  },
+  activityItemBodyContainer: {
+    width: "100%",
+    backgroundColor: "#EDEFFB",
+    borderRadius: 6,
+    padding: 8,
+    marginTop: 10,
+  },
+  activityTitleContainer: {
+    display: "flex",
+    flexDirection: "row",
   },
 });
 
